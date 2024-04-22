7b1126bd009ea34cd5d0e4f0850aa936ff64042c
Aditya Ranjan <136574821+Aditya-Ranjan1234@users.noreply.github.com>
2024-04-23 01:04:41 +0530

    Update Note.jsx

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DeleteIcon from "@material-ui/icons/Delete";
+import DeleteIcon from "@material-ui/icons/delete";
 
 function Note(props) {
   function handleClick() {
